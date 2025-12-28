93953b905aab5d6821cc53ad7f77fe215f4b09b9
hoanganhchung1982 <43825710+hoanganhchung1982@users.noreply.github.com>
2025-12-28 22:20:50 +0700

    Delete api/hello.ts

--- api/hello.ts
+++
@@ -1,8 +0,0 @@
-// Đây là code chạy trên Server (Backend)
-export const config = { runtime: 'edge' }; // Tối ưu cho Vercel
-
-export default async (req: Request) => {
-  return new Response(JSON.stringify({ message: "Chào bạn từ Backend!" }), {
-    headers: { "Content-Type": "application/json" },
-  });
-};
